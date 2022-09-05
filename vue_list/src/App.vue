49588d3602b95e2aadf8c42096521a2d614f00ff
patd <pdoran@gmail.com>
2022-09-05 14:49:24 -0400

    Add vue component to the mix

--- vue_list/src/App.vue
+++ vue_list/src/App.vue
@@ -2,11 +2,11 @@
 import { ref, onMounted } from "vue";
 
 // reactive state
-let wordPressGraphQL = ref("https://patrickdoran.com/headless/graphql")
+let wordPressGraphQL = ref("https://patrickdoran.com/headless/graphql");
 let entries = ref([]);
 
-const getPosts = (async () =>{
- // Queries Headless WordPress site configured for GraphQL
+const getPosts = async () => {
+  // Queries Headless WordPress site configured for GraphQL
   const res = await fetch(wordPressGraphQL.value, {
     method: "POST",
     headers: {
@@ -33,26 +33,29 @@ const getPosts = (async () =>{
 
   // Wait for the results to load
   let results = await res.json();
+
   // Update Vue state
   return results.data.posts.edges;
-})
+};
 
-// When the component is mounted, update state with the 
+// When the component is mounted, update state with the
 // headless Wordpress GraphQL post data
 onMounted(async () => {
-  entries.value = await getPosts()
+  entries.value = await getPosts();
 });
 </script>
 
 <template>
-  <div>
-    <ul v-for="entry in entries" :key="entry.node.postId">
-      <li>
-        <p>{{ entry.node.title }}</p>
-        <p>{{entry.node.excerpt }}</p>
-        <p>{{ entry.node.content }}</p>
-        <p><time datetime={entry.node.date}>{{entry.node.date.substring(0, 4)}}</time></p>
-        </li>
-    </ul>
-  </div>
+  <ul>
+    <li v-for="entry in entries" :key="entry.node.postId">
+      <p>{{ entry.node.title }}</p>
+      <p>{{ entry.node.excerpt.replace(/(<([^>]+)>)/gi, "") }}</p>
+      <p>{{ entry.node.content.replace(/(<([^>]+)>)/gi, "") }}</p>
+      <p>
+        <time datetime="{entry.node.date}">{{
+          entry.node.date.substring(0, 4)
+        }}</time>
+      </p>
+    </li>
+  </ul>
 </template>
